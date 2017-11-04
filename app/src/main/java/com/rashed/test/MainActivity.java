d59936f1a9d1848ec28875d61e3b92c4081c312d
RDALS <32703543+RDALS@users.noreply.github.com>
2017-11-04 14:42:41 +0000

    Try1

--- app/src/main/java/com/rashed/test/MainActivity.java
+++ app/src/main/java/com/rashed/test/MainActivity.java
@@ -2,6 +2,9 @@ package com.rashed.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button b1 = (Button)findViewById(R.id.b1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View v) {
+
+                TextView Byetxt =(TextView) findViewById(R.id.Byetxt);
+
+                Byetxt.setText("ok");
+
+
+
+
+
+            }
+
+
+
+                              }
+
+
+
+        );
+
+
+
+        TextView Byetxt =(TextView) findViewById(R.id.Byetxt);
+
+
+
+
     }
 }
